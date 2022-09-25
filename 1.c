a8e768ee14e45761cba34fa5dc3f2b837baec8be
Satyam1722 <72600713+Satyam1722@users.noreply.github.com>
2022-09-25 16:49:01 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number of times : ");
+    scanf("%d",&n);
+    while(n--)
+    printf("MySirG \n");
+
+    return 0;
+}
---
+++ 9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("enter the limit : ");
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+   printf("%d ",i*i*i); 
+
+    return 0;
+}
